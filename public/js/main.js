57ebc685df765b3d455f052b98e9c169b3e434bb
Kristie Rodriguez <kristierodriguez@Kristies-MacBook-Pro.local>
2017-07-12 10:29:17 -0500

    Adding changes to Simple Simon functions and CSS.

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
  "use strict";
 
-        	var boxes = ["one", "two", "three", "four"];
-        	// var random = [];
+        	var boxes = ["red", "green", "blue", "yellow"];
+        
         	var liteId = [];
         	var lite;
-        	var userClicked = [];
+        	var userClicked = "";
+        	var userClickedIndex = 0
         	var i;
-        	var colorArray = 0;
-        	var count = 1;
-        	var on;
-        	var off;
+        	
+        	var count = 0;
+        
         	var stopInterval;
 
         	function random() {
@@ -35,6 +35,7 @@ $(document).ready(function() {
         	$("#start").click(function(){
         		random();
         		flashSequence();
+        		console.log(liteId);
         		// $('.box').animate( {
         		// 	opacity: "1",
         		// }, 800).animate({
@@ -44,139 +45,33 @@ $(document).ready(function() {
 				// gamePlay();
 			});
 
+			$(".box").click(function(){
+				userClicked = this.id;
+				if (userClicked === liteId[userClickedIndex]) {
+					userClickedIndex += 1;
+					if (userClickedIndex === liteId.length){
+						userClickedIndex = 0;
+						count++;
+						updateCount();
+						random();
+						flashSequence();
+
+					console.log(liteId);
+					console.log(userClicked + " " + userClickedIndex);
+					}
+				} else{
+					alert("Game Over!");
+				}
+
+			});
+
+			function updateCount(){
+				$("#count").text(count);
+			}
+
+
 
         	
 
-	        function gamePlay() {
-	        	if (count <= 10) {
-	        		off = 400;
-	        		on = 800;
-	        	} else {
-	        		off = 250;
-	        		on = 500;
-	        	}
-	        };
-
-		        var stopInterval = setInterval(function(){
-		        	if (random[colorArray] == 1) {
-
-		        		lite = "activeBox1";
-		        		$("#one").addClass(lite);
-		        		$("#audio1")[0].play();
-		        		liteId.push(1);
-
-		        		setTimeOut(function(){
-		        			$("#one").removeClass(lite);
-
-		        		}, off);
-		        	}
-
-		        	else if(random[colorArray] ==2) {
-		        		
-		        		lite = "activeBox2";
-		        		$("#two").addClass(lite);
-		        		$("#audio2")[0].play();
-		        		liteId.push(2);
-
-			        	setTimeOut(function(){
-			        		$("#two").removeClass(lite);
-
-			        	}, off);
-
-			        }
-
-		        	else if(random[colorArray] ==3) {
-
-		        		lite = "activeBox3";
-		        		$("#three").addClass(lite);
-		        		$("#audio3")[0].play();
-		        		liteId.push(3);
-
-			        	setTimeOut(function(){
-			        		$("#three").removeClass(lite);
-
-			        	}, off);
-
-		        	}
-
-		        	else if(random[colorArray] ==4) {
-		        		lite = "activeBox4";
-		        		$("#four").addClass(lite);
-		        		$("#audio4")[0].play();
-		        		liteId.push(4);
-
-			        	setTimeOut(function(){
-			        		$("#four").removeClass(lite);
-
-			        	}, off);
-		        	}
-
-		        	colorArray ++;
-		        		if (colorArray = count) {
-		        			clearInterval(stopInterval);
-		        		}
-
-		        	}, on);
-		    });
-	
-			function checkInput(){
-				if(liteId.length == userClicked.length) {
-					
-					if (liteId.join() == userClicked.join()){
-						if (count == 20) {
-							setTimeOut(function(){
-								alert ("You Win!!!");
-							location.reload();
-							},1000);
-
-						} 
-
-						else {
-							setTimeOut(function(){
-								$("#count").text(count + 1);
-								count ++;
-								liteId = [];
-								userClicked = [];
-								colorArray = 0;
-								gamePlay();
-							},1000);
-						}
-					} 
-
-					else {
-						if (strict == 1) {
-							location.reload();
-
-						} 
-
-						else {
-							setTimeOut(function(){
-								$("#count").text("!!");
-								liteId = [];
-								userClicked = [];
-								colorArray = 0;
-								gamePlay();
-							}, 1000);
-						}
-					}
-				}
-			}
-
-			$("#on").click(function(){
-				$("#count").text("");
-				for (i = 0; i < 20; i++){
-					random[i] = math.ceil(math.random() *4);
-				}
-			});
-
-			$("#strict").click(function(){
-				strict = 1;
-				});
-
-			// $("#start").click(function(){
-			// 	$("#count").text(count);
-			// 	// gamePlay();
-			// });
-
 			
-         //});
+         });
